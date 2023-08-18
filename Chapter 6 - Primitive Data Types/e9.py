263ebbbff7c739f6a68f54dce0f21f3877018eb7
C0deMinds <codemindslearning@gmail.com>
2023-08-18 18:50:53 -0400

    fixed chapter 6 e9 mistake

--- Chapter 6 - Primitive Data Types/e9.py
+++ Chapter 6 - Primitive Data Types/e9.py
@@ -2,7 +2,7 @@
 Exercise 9: Ask the user to input a sentence. 
 Print the sentence in reverse order using string slicing and negative indexes
 
-Line 7 Explanation:
+Line 12 Explanation:
 There is a third parameter you can use when string slicing which dictates how much you increment by, and -1 means backwords. 
 For example, string[1:1:2] means that every 2nd character will print. If it's 1, you can leave it empty and just do string[::2]
 Try it out yourself!
